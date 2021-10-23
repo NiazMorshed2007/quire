20aa0dd0267697b97881a7a64e54ae0e3bd7bcb3
NiazMorshed2007 <niazmorshed2007@gmail.com>
2021-10-23 10:38:32 +0600

    updated

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCGFXpnZSbI1f-NdIVqxPhsu47KSXTDfL0",
+    authDomain: "quire-57f05.firebaseapp.com",
+    projectId: "quire-57f05",
+    storageBucket: "quire-57f05.appspot.com",
+    messagingSenderId: "981834485878",
+    appId: "1:981834485878:web:10441cb9c7a30ddc37cf57"
+};
+
+export const app = initializeApp(firebaseConfig);
