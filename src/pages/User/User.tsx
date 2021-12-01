b6e9896a1027dad0fe2f49996f435b6bd5a3cb7e
NiazMorshed2007 <niazmorshed2007@gmail.com>
2021-12-01 20:47:57 +0600

    co

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useEffect } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import { User } from "../../context/user";
 import { Route } from "react-router-dom";
@@ -8,9 +8,22 @@ import TasksPage from "../Task/TasksPage";
 
 const UserSpace: FC = () => {
   const { user }: any = useContext(User);
+  const [url, setUrl] = useState<string>("");
+  const [file, setFile] = useState<File>();
   useEffect(() => {
     document.title = `${user.user && user.user.displayName} | Quire`;
   }, [user]);
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setUrl(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setUrl("");
+    }
+  }, [file]);
   return (
     <>
       {user.user && (
@@ -27,6 +40,21 @@ const UserSpace: FC = () => {
             <Overview>
               <BaseInfo type="USER" title={user.user.displayName} />
             </Overview>
+            <input
+              accept="image/*"
+              type="file"
+              onChange={(e) => {
+                const f = e.target.files && e.target.files[0];
+                if (f && f?.type.substr(0, 5) === "image") {
+                  setFile(f);
+                }
+              }}
+            />
+            <img
+              style={{ width: "80px", height: "80px", borderRadius: "50%" }}
+              src={url}
+              alt="a image"
+            />
           </Route>
           <Route path="/u/my_tasks">
             <TasksPage type="USER" />
